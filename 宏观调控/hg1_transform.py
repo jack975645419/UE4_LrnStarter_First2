7377ea9c58246d4e59d234aa2b8352def283ba85
jack975645419 <jack13726560352@126.com>
2022-12-26 10:34:49 +0800

    宏观调控文件夹与测试样例

---
+++ 宏观调控/hg1_transform.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+'''脚本说明： 
+'''
+
+import os, sys
+
+# str版本：
+def read_file_plain(filepath):
+    outf = open(filepath, "r", encoding='utf-8', errors='ignore') 
+    content = outf.read()
+    outf.close()
+    return content
+    
+def write_file(filepath, content):
+    outf = open(filepath, "w", encoding='utf-8', errors='ignore')
+    outf.write(content)
+    outf.close()
+
+content = read_file_plain("宏观调控.py")
+
+content = content.replace("${{BUILD_NUM}}", sys.argv[1])
+content = content.replace("${{PIPELINE_ID}}", sys.argv[2])
+content = content.replace("${{NODES}}", sys.argv[3])
+content = content.replace("${{DELAY_START}}", sys.argv[4])
+content = content.replace("${{LOCK}}", sys.argv[5])
+write_file(sys.argv[6], content)
+
---
+++ 宏观调控/hg3_run【hg2和hg3是一组样例，观察到最后json是0000就好】.cmd
@@ -0,0 +1,22 @@
+
+start python hg_1.py
+TIMEOUT /T 5
+start python hg_2.py
+TIMEOUT /T 5
+start python hg_3.py
+TIMEOUT /T 5
+start python hg_4.py
+TIMEOUT /T 5
+start python hg_5.py
+
+TIMEOUT /T 10
+
+start python hg_2f.py
+TIMEOUT /T 5
+start python hg_1f.py
+TIMEOUT /T 5
+start python hg_4f.py
+TIMEOUT /T 5
+start python hg_3f.py
+TIMEOUT /T 5
+start python hg_5f.py
